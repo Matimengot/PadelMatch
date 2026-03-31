79c44921501ef29677138f6f842be977c4be532f
Matimengot <matimengot@gmail.com>
2026-03-31 13:26:55 -0300

    revert: remove hardcoded supabase keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'https://wndvqnviyieenbkraoqk.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InduZHZxbnZpeWllZW5ia3Jhb3FrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4OTg4NDIsImV4cCI6MjA5MDQ3NDg0Mn0.gm6wGdF3okmKEdJoDzazrgY1AT7RLDLApQluFEtHuMQ'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
